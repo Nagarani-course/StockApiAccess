ec30d45410e52111d6aa9ba171782993fd0148e5
Nagarani-course <gmaporur@gmail.com>
2025-11-14 17:28:38 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let express = require('express');
+let app = express();
+let stockRouter = require('./routes/stockRoutes.js');
+
+app.use(express.json());
+
+app.use('/stocks', stockRouter);
+
+
+//http://localhost:3000/stocks/allStocks
+app.listen(9090, () => console.log("server is listening on the 9090 port"));
