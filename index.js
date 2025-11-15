53c15ac7cfb69022a4e90e22fffdd092017b37cf
Nagarani-course <gmaporur@gmail.com>
2025-11-15 16:45:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let express = require('express');
-let app = express();
-let stockRouter = require('./routes/stockRoutes.js');
+const stockService = require('../services/stockServices.js');
 
-app.use(express.json());
+let getAllStockDetails = async(request, response) => {
+    try {
+        console.log("Inside controller: fetching all stcoks...");
+        const stocks = await stockService.fetchAllStockDetails();
+       // console.log(stocks);
+        //response.status(200).json({ messsage: "Stock fetched Successully in controller", data: stocks.data });
+        response.status(200).json( stocks.data );
+    } catch(error) {
+        console.log(error);
+        response.status(200).json({ messsage: "Error Fetching Stocks in controller...", data: error.messsage });   
+    }
+}
 
-app.use('/stocks', stockRouter);
-
-
-//http://localhost:3000/stocks/allStocks
-app.listen(9090, () => console.log("server is listening on the 9090 port"));
+module.exports = { getAllStockDetails };
