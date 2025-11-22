f850b229bb59ac9053369d28708821a77f734ba9
Nagarani-course <gmaporur@gmail.com>
2025-11-22 17:04:36 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let dotenv = require('dotenv');
 let dbconfig = require('./config/dbconfig.js');
+const router = express.Router();
+let cookieParser = require('cookie-parser');
+
+
 
 dotenv.configDotenv();
 
@@ -10,11 +14,33 @@ let app = express();
 let stockRouter = require('./routes/stockRoutes.js');
 
 app.use(express.json());
+app.set('json spaces', 2); 
+//dbconfig.ConnectDB()
 
-dbconfig.ConnectDB();
+//app.use('/stocks', stockRouter);
 
-app.use('/stocks', stockRouter);
+let allRouters=[
+    ["/api/stocks",stockRouter]
+]
 
+app.use(express.json())
+    .use(cookieParser());
 
-//http://localhost:3000/stocks/allStocks
+allRouters.forEach(it=>app.use(it[0],it[1]))
+
 app.listen(PORT, () => console.log(`server is listening on the ${PORT} port`));
+
+app.get("/", (req, res) =>{
+    let routes = [
+        ["/",app._router],
+        ...allRouters
+    ]
+    let routesJs = {}
+    routes.forEach(it=>{
+        let key = it[0]
+        let value  = it[1]?.stack.filter(r => r.route).map(r => ({method: Object.keys(r.route.methods)[0].toUpperCase(), path: r.route.path}));
+        routesJs[key]=value
+    })
+       res.json(routesJs)
+} )
+
