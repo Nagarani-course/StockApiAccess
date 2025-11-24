4b65052e33da87f4e5c001593b84c543daac5d8e
Nagarani-course <gmaporur@gmail.com>
2025-11-24 06:34:34 +0000

    Add files via upload

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllStockDetails, getAllStockNames, getCount, storeStock } = require('../controllers/stockController.js');
+const { getAllStockDetails, getAllStockNames, getCount, storeStock, getOneStockDetails, getStockPurchaseDetiail } = require('../controllers/stockController.js');
 
 router.get('/allStocks', getAllStockDetails);
 router.get('/allStockNames', getAllStockNames);
 router.get('/stockCount', getCount);
-router.post('/storeStock', storeStock);
+router.post('/storeStockPurchase', storeStock);
+router.get('/oneStockDetail', getOneStockDetails);
+router.get('/stockPurchaseDetail', getStockPurchaseDetiail);
 
 module.exports = router;
-
